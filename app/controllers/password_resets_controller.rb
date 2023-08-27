78e64c031c393751d17856b9fe2ecf219d129339
anhthai <nguyen.doan.anh.thai@sun-asterisk.com>
2023-08-27 23:58:20 +0700

    Chapter 12

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,63 @@
+class PasswordResetsController < ApplicationController
+  before_action :get_user, :valid_user, :check_expiration, only: %i(edit update)
+  def new; end
+
+  def edit; end
+
+  def create
+    @user = User.find_by email: params.dig(:password_reset, :email)&.downcase
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = t "forgot_password.email.send"
+      redirect_to root_url
+    else
+      flash.now[:danger] = t "forgot_password.email.not_found"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, t("forgot_password.password.not_found"))
+      render :edit, status: :unprocessable_entity
+    elsif @user.update user_params
+      reset_session
+      log_in @user
+      @user.update_column :reset_digest, nil
+      flash[:success] = t "forgot_password.password.success"
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+    return if @user
+
+    flash[:danger] = t "forgot_password.user.not_found"
+    redirect_to root_url
+  end
+
+  # Confirms a valid user.
+  def valid_user
+    return if @user.activated? && @user.authenticated?(:reset, params[:id])
+
+    flash[:danger] = t "forgot_password.user.not_actived"
+    redirect_to root_url
+  end
+
+  # Checks expiration of reset token.
+  def check_expiration
+    return unless @user.password_reset_expired?
+
+    flash[:danger] = t "forgot_password.expired"
+    redirect_to new_password_reset_url
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_25_093718) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_27_151133) do
   create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_25_093718) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
