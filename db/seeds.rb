3c7c04f3a42ee51332e15fbf28f1d35a4beaa32b
anhthai <nguyen.doan.anh.thai@sun-asterisk.com>
2023-08-28 14:07:56 +0700

    Chapter 12

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.create!(name: "thai",
-             email: "thai@gmail.com",
+             email: "thai1@gmail.com",
              password: "123456",
              password_confirmation: "123456",
              admin: true,
